8143c5c9e944cc7ebb662f74ccf89415a0407491
KwonNam Son <kwon37xi@gmail.com>
2016-08-28 16:30:33 +0900

    fix compile errors

--- src/main/java/kr/pe/kwonnam/hibernate4localdatetime/LocalTimeUserType.java
+++ src/main/java/kr/pe/kwonnam/hibernate4localdatetime/LocalTimeUserType.java
@@ -1,8 +1,82 @@
 package kr.pe.kwonnam.hibernate4localdatetime;
 
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.usertype.EnhancedUserType;
 
 import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LocalTimeUserType implements EnhancedUserType, Serializable {
+    @Override
+    public String objectToSQLString(Object value) {
+        return null;
+    }
+
+    @Override
+    public String toXMLString(Object value) {
+        return null;
+    }
+
+    @Override
+    public Object fromXMLString(String xmlValue) {
+        return null;
+    }
+
+    @Override
+    public int[] sqlTypes() {
+        return new int[0];
+    }
+
+    @Override
+    public Class returnedClass() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object x, Object y) throws HibernateException {
+        return false;
+    }
+
+    @Override
+    public int hashCode(Object x) throws HibernateException {
+        return 0;
+    }
+
+    @Override
+    public Object nullSafeGet(ResultSet rs, String[] names, SessionImplementor session, Object owner) throws HibernateException, SQLException {
+        return null;
+    }
+
+    @Override
+    public void nullSafeSet(PreparedStatement st, Object value, int index, SessionImplementor session) throws HibernateException, SQLException {
+
+    }
+
+    @Override
+    public Object deepCopy(Object value) throws HibernateException {
+        return null;
+    }
+
+    @Override
+    public boolean isMutable() {
+        return false;
+    }
+
+    @Override
+    public Serializable disassemble(Object value) throws HibernateException {
+        return null;
+    }
+
+    @Override
+    public Object assemble(Serializable cached, Object owner) throws HibernateException {
+        return null;
+    }
+
+    @Override
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return null;
+    }
 }
