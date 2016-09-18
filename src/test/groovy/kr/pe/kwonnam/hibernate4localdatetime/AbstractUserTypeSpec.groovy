b42e8ac2e2f0f10376cbea652922fe2977805fbc
KwonNam Son <kwon37xi@gmail.com>
2016-09-18 16:52:01 +0900

    add logback configuration

--- src/test/groovy/kr/pe/kwonnam/hibernate4localdatetime/AbstractUserTypeSpec.groovy
+++ src/test/groovy/kr/pe/kwonnam/hibernate4localdatetime/AbstractUserTypeSpec.groovy
@@ -20,9 +20,8 @@ abstract class AbstractUserTypeSpec extends Specification {
 
         configuration.addAnnotatedClass(Article)
 
-        configuration.setProperty("hibernate.show_sql", "true");
+        configuration.setProperty("hibernate.show_sql", "false");
         configuration.setProperty("hibernate.format_sql", "true");
-        configuration.setProperty("hibernate.hbm2ddl.auto", "update");
         configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
         configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1");
